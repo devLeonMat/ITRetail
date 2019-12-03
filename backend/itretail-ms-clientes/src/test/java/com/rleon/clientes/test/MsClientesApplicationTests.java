c79425b6637cae0d25f610942829384114278686
javier <drakecf2@gmail.com>
2019-12-02 19:33:05 -0500

    add projects

---
+++ backend/itretail-ms-clientes/src/test/java/com/rleon/clientes/test/MsClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rleon.clientes.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
